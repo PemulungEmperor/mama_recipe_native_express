9fd8286d4e91e77f6bd5fd94eee26ef74ed4acb5
PemulungEmperor <yuliuslebelamak@yahoo.com>
2023-07-01 14:00:13 +0700

    res entry point

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-unused-vars */
-const express = require('express');
-const bodyParser = require('body-parser'); // ISI DATA DARI POSTMAN
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const morgan = require('morgan');
-const xss = require('xss-clean');
+const express = require("express");
+const bodyParser = require("body-parser"); // ISI DATA DARI POSTMAN
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const xss = require("xss-clean");
 
 //path
-const path = require('path');
+const path = require("path");
 // port
 const port = process.env.PORT || 5000;
 
 // router
-const userRouter = require('./src/router/user.router');
-const productRouter = require('./src/router/product.router');
+const userRouter = require("./src/router/user.router");
+const productRouter = require("./src/router/product.router");
 
 const app = express();
 // app.use(express.static(path.join(__dirname, './thefrontend/build')))
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 // app.use(cors())
-app.use(cors({credentials: true}));
-app.use(morgan('combined'));
+app.use(cors({ credentials: true }));
+app.use(morgan("combined"));
 
 //CSP
 app.use(helmet());
@@ -40,6 +40,10 @@ app.use(productRouter);
 
 app.listen(port, () => {
   console.log(`SERVER LISTEN ON PORT ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, This is API for mama recipe native (yulius batch 10");
 });
 
 module.exports = app;
